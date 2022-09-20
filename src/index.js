f9e8b1ea7198de6e9e5591955d9f53b12683c5fe
naijatechgirle <kellyeluagu@gmail.com>
2022-09-20 12:53:37 +0100

    instllaing dependencies and packages

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@
 // console.log(process.env)
 let showDiv = document.getElementById('showDiv');
 
+// console.log(process.env.API_KEY);
+
 let myFunction =()=> {
   
-let myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-myHeaders.append("Authorization", "Basic YXBpOmtleS01MDYwNmIxODViMWU2YjlhNzNjODQyZmNhYjM4NjQ2MQ==");
+// let myHeaders = new Headers();
+// myHeaders.append("Content-Type", "application/json");
+// myHeaders.append("Authorization", "Basic YXBpOmtleS01MDYwNmIxODViMWU2YjlhNzNjODQyZmNhYjM4NjQ2MQ==");
 
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
-};
+// var requestOptions = {
+//   method: 'GET',
+//   headers: myHeaders,
+//   redirect: 'follow'
+// };
 
+
+
+console.log(process.env)
 fetch("https://api.mailgun.net/v3/app.myxalary.com/events", requestOptions)
   .then(response => response.text())
       .then(result => console.log(result))
